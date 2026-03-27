a70e8708242184390bb8e46663fb907b31750ce6
MykolaLozanDevelopment <mykola.lozan.development@gmail.com>
2026-03-27 19:49:22 +0200

    FIX-2 Solution task Split integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,54 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 8
-    parts = 3
-    result = split_integer(value, parts)
-    assert isinstance(result, list)
-    assert len(result) == parts
-    assert sum(result) == value
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
+    assert split_integer(8, 3) == [2, 3, 3]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 100
-    parts = 5
-    result = split_integer(value, parts)
-    assert isinstance(result, list)
-    assert result == [20, 20, 20, 20, 20]
-    assert len(result) == parts
-    assert sum(result) == value
+    assert split_integer(100, 5) == [20, 20, 20, 20, 20]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 30
-    parts = 1
-    result = split_integer(value, parts)
-    assert isinstance(result, list)
-    assert result == [30]
-    assert len(result) == 1
-    assert sum(result) == value
+    assert split_integer(30, 1) == [30]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 17
-    parts = 4
-    result = split_integer(value, parts)
-    assert isinstance(result, list)
-    assert result == sorted(result)
-    assert len(result) == parts
-    assert sum(result) == value
-    assert max(result) - min(result) <= 1
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 10
-    parts = 20
-    result = split_integer(value, parts)
-    assert isinstance(result, list)
-    assert len(result) == parts
-    assert sum(result) == value
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
-    assert all(isinstance(x, int) for x in result)
+    assert split_integer(10, 20) == [0] * 10 + [1] * 10
